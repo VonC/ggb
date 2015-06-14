18cddb9fe779ffb3f48e07670b3f8f53bb955f1f
VonC <vonc@laposte.net>
2015-06-15 00:31:15 +0200

    ggb.go: add addsub command, and help usage

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+import flag "github.com/spf13/pflag"
+
+var help bool
+
+func init() {
+	flag.BoolVarP(&help, "help", "h", false, "ghref usage")
+}
 
 func main() {
-	fmt.Println("ggb")
+	flag.Parse()
+	if help {
+		usage()
+		os.Exit(0)
+	}
+	fmt.Printf("ggb: ")
+	if len(flag.Args()) == 0 {
+		build()
+	} else {
+		processCommands(flag.Args())
+	}
+}
+
+func processCommands(args []string) {
+	switch args[0] {
+	case "addsub":
+		addsub(args)
+	default:
+		fmt.Print("Unknown command " + args[0])
+	}
+}
+
+func addsub(args []string) {
+	fmt.Printf("addsub %v", args)
+}
+
+func build() {
+	fmt.Print("build to be done")
+}
+
+func usage() {
+	fmt.Print(`ggb [-h],
+builds a go project with git submodule dependencies`)
 }
