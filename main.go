e00a8ce8f0c85d5022c3f0d07771da35ce9c2d61
VonC <vonc@laposte.net>
2015-06-16 22:40:43 +0200

    main.go: calls cmd.RunCommand(), provides build() function

--- main.go
+++ main.go
@@ -3,40 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"./cmd"
 )
-import flag "github.com/spf13/pflag"
-
-var help bool
-
-func init() {
-	flag.BoolVarP(&help, "help", "h", false, "ghref usage")
-}
 
 func main() {
-	flag.Parse()
+	fmt.Printf("ggb: ")
+	err := cmd.RunCommand(os.Args)
+	if err != nil {
+		fmt.Printf("%s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func build(args []string) error {
+	fmt.Printf("build to be done with args '%v'", args)
+	return nil
+}
+
+func checkGlobalFlag() {
 	if help {
 		usage()
 		os.Exit(0)
 	}
-	fmt.Printf("ggb: ")
-	if len(flag.Args()) == 0 {
-		build()
-	} else {
-		processCommands(flag.Args())
-	}
-}
-
-func processCommands(args []string) {
-	switch args[0] {
-	case "addsub":
-		addsub(args)
-	default:
-		fmt.Print("Unknown command " + args[0])
-	}
-}
-
-func build() {
-	fmt.Print("build to be done")
 }
 
 func usage() {
