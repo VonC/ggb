4cbdde99b14655ca9c2a95229883b5cae39b7eb4
VonC <vonc@laposte.net>
2015-06-16 22:39:32 +0200

    cmds.go: defines cmdggb, with global flags

--- cmds.go
+++ cmds.go
@@ -2,7 +2,10 @@ package main
 
 import "./cmd"
 
-var commands map[string]*cmd.Command
+var debug bool
+var verbose bool
+var help bool
+var version bool
 
 func init() {
 	initCommands()
@@ -13,11 +16,17 @@ func initCommands() {
 }
 
 func initCommandGgb() {
-	cmd := cmd.NewCommand("ggb",
+	cmdggb := cmd.NewCommand("ggb",
 		"ggb [cmd]",
 		"builds a go project with git submodule dependencies management",
 		`ggb builds, 
 while ggb deps offers dependency management as git submodules`,
-		nil)
-	commands["ggb"] = cmd
+		build, nil)
+
+	gfs := cmdggb.GFS()
+	gfs.BoolVarP(&help, "help", "h", false, "ggb usage")
+	gfs.BoolVarP(&verbose, "verbose", "v", false, `display a verbose output
+		not suited for batch usage`)
+	gfs.BoolVarP(&debug, "debug", "d", false, "output debug informations (not for batch usage)")
+	gfs.BoolVarP(&version, "version", "V", false, "display ggb version")
 }
