8f518ca48d817cf8510b1cbdd49116e736f27e89
VonC <vonc@laposte.net>
2015-06-18 00:17:55 +0200

    cmds.go: uses SetGFS for global flags on ggb command

--- cmds.go
+++ cmds.go
@@ -1,6 +1,10 @@
 package main
 
-import "./cmd"
+import (
+	flag "github.com/spf13/pflag"
+
+	"github.com/VonC/ggb/cmd"
+)
 
 var debug bool
 var verbose bool
@@ -22,11 +26,11 @@ func initCommandGgb() {
 		`ggb builds, 
 while ggb deps offers dependency management as git submodules`,
 		build, nil)
-
-	gfs := cmdggb.GFS()
-	gfs.BoolVarP(&help, "help", "h", false, "ggb usage")
-	gfs.BoolVarP(&verbose, "verbose", "v", false, `display a verbose output
+	cmdggb.SetGFS(func(gfs *flag.FlagSet) {
+		gfs.BoolVarP(&help, "help", "h", false, "ggb usage")
+		gfs.BoolVarP(&verbose, "verbose", "v", false, `display a verbose output
 		not suited for batch usage`)
-	gfs.BoolVarP(&debug, "debug", "d", false, "output debug informations (not for batch usage)")
-	gfs.BoolVarP(&version, "version", "V", false, "display ggb version")
+		gfs.BoolVarP(&debug, "debug", "d", false, "output debug informations (not for batch usage)")
+		gfs.BoolVarP(&version, "version", "V", false, "display ggb version")
+	})
 }
