11c73e536f33ebb4f275bd35a9a420071001464b
VonC <vonc@laposte.net>
2015-06-21 21:59:46 +0200

    project.go: refactor getPathForExe() with aliases or .bat

--- prj/project.go
+++ prj/project.go
@@ -3,9 +3,12 @@ package prj
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"regexp"
+	"runtime"
 	"strings"
 )
 
@@ -21,8 +24,8 @@ func GetProject() (*Project, error) {
 		gdir, gerr := Git("rev-parse --git-dir")
 		gdir = strings.TrimSpace(gdir)
 		// fmt.Printf("ko '%s' '%s'", gdir, gerr)
-		if gerr != "" {
-			return nil, fmt.Errorf("'%s': '%s'", wd, gerr)
+		if gerr != nil {
+			return nil, gerr
 		}
 		if gdir != ".git" {
 			project.rootFolder = gdir[:len(gdir)-5]
@@ -46,34 +49,64 @@ var goPath string
 var wd string
 
 func init() {
+	gitPath = getPathForExe("git")
+	goPath = getPathForExe("go")
 	var err error
-	gitPath, err = exec.LookPath("git")
-	if err != nil {
-		log.Fatal("git must be installed")
-	}
-	// fmt.Println(gitPath)
-	goPath, err = exec.LookPath("go")
-	if err != nil {
-		log.Fatal("go must be installed")
-	}
-	goPath = `C:\prgs\go\go1.4.2.windows-amd64\bin\go.exe`
 	wd, err = os.Getwd()
 	if err != nil {
 		log.Fatal("Working directory not accessible")
 	}
 }
 
+func getPathForExe(exe string) string {
+	var err error
+	var path = ""
+	if path, err = exec.LookPath(exe); err != nil {
+		aliases := ""
+		if runtime.GOOS == "windows" {
+			aliases, err = execcmd("doskey", "/macros")
+		} else {
+			aliases, err = execcmd("alias", "")
+		}
+		r := regexp.MustCompile(`(?m)^` + exe + `=(.*)\s+[\$%@\*].*$`)
+		sm := r.FindAllStringSubmatch(aliases, 1)
+		if len(sm) != 1 || len(sm[0]) != 2 {
+			log.Fatalf("Unable to find '%s' path in aliases '%s'", exe)
+		}
+		return sm[0][1]
+	}
+	if runtime.GOOS == "windows" {
+		if strings.HasSuffix(path, ".bat") {
+			bat, err := ioutil.ReadFile(path)
+			if err != nil {
+				log.Fatalf("Unable to read '%s' for '%s'", path, exe)
+			}
+			bats := string(bat)
+			r := regexp.MustCompile(`(?m)^\s*?(.*)\s+[\$%@\*].*$`)
+			sm := r.FindAllStringSubmatch(bats, 1)
+			if len(sm) != 1 || len(sm[0]) != 2 {
+				log.Fatalf("Unable to find '%s' path in file '%s'", exe, path)
+			}
+			return sm[0][1]
+		}
+	}
+	if path == "" {
+		log.Fatalf("Unable to get path for '%s'", exe)
+	}
+	return path
+}
+
 // Construct an *exec.Cmd for `git {args}` with a workingDirectory
-func Git(cmd string) (string, string) {
+func Git(cmd string) (string, error) {
 	return execcmd(gitPath, cmd)
 }
-func Golang(cmd string) (string, string) {
+func Golang(cmd string) (string, error) {
 	os.Setenv("GOPATH", project.rootFolder+`/deps`)
 	os.Setenv("GOBIN", project.rootFolder+`/bin`)
 	return execcmd(goPath, cmd)
 }
 
-func execcmd(exe, cmd string) (string, string) {
+func execcmd(exe, cmd string) (string, error) {
 	args := strings.Split(cmd, " ")
 	c := exec.Command(exe, args...)
 	c.Dir = project.rootFolder
@@ -83,7 +116,9 @@ func execcmd(exe, cmd string) (string, string) {
 	c.Stderr = &berr
 	err := c.Run()
 	if err != nil {
-		fmt.Printf("Unable to run '%s %s' in '%s': err '%s'\n", exe, cmd, wd, err.Error())
+		return bout.String(), fmt.Errorf("Unable to run '%s %s' in '%s': err '%s'\n'%s'", exe, cmd, wd, err.Error(), berr.String())
+	} else if berr.String() != "" {
+		return bout.String(), fmt.Errorf("Warning on run '%s %s' in '%s': '%s'", exe, cmd, wd, berr.String())
 	}
-	return bout.String(), berr.String()
+	return bout.String(), nil
 }
